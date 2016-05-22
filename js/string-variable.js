5b5501e9c3b3e8a2667f280c259a68172b9e3ab6
MerylHarden <merylharden@gmail.com>
2016-05-22 06:59:01 -0700

    variable practice

---
+++ js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Meryl';
+message = 'Good job with your coding!';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
